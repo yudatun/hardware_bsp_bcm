65cde15eb86b5aa71cb97e3c6fd580a7e87de84d
liminghao <liminghao@xiaomi.com>
2016-12-05 16:24:14 +0800

    bluetooth: add the firmwares of raspberrypi bluetooth modules.
    N/A
    
    Signed-off-by: liminghao <liminghao@xiaomi.com>

---
+++ peripheral/bluetooth/bcm43438/peripheral.mk
@@ -0,0 +1,25 @@
+#
+# Copyright 2016 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+BLUETOOTH_BSP_SRC = hardware/bsp/bcm/peripheral/bluetooth/bcm43438
+
+BLUETOOTH_DRIVER_HAL_PERIPHERAL := bcm43438
+
+BOARD_SEPOLICY_DIRS += $(BLUETOOTH_BSP_SRC)/sepolicy
+
+# Enable bluetooth
+BOARD_HAVE_BLUETOOTH_BCM := true
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(BLUETOOTH_BSP_SRC)
